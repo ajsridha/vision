e8a069b3d5ef9fa910b2d31f19588b9d9dd99036
Andrew McIntosh <amcintosh@freshbooks.com>
2018-05-09 10:34:54 -0400

    Setup packages

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 PACKAGE_NAME = "vision"
-VERSION = "0.0.1"
 
 setup(
     name=PACKAGE_NAME,
-    version=VERSION,
+    version=open('vision/VERSION').readlines()[0].strip(),
     description='Simple receipt parser',
     author="Anandh Sridharan",
-    author_email="anandhs@freshbooks.com"
+    author_email="anandhs@freshbooks.com",
+    packages=find_packages(exclude=['*.test', '*.test.*']),
+    include_package_data=True
 )
