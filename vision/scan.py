c874355e6701001ffc6e8b9ef9d690cdcd651456
Anandh Sridharan <anandhs@freshbooks.com>
2018-05-09 13:27:33 -0400

    Break apart the word class.

--- vision/scan.py
+++ vision/scan.py
@@ -4,40 +4,6 @@ from google.cloud import vision
 from vision.constants import GRAND_TOTAL_FIELDS, SUBTOTAL_FIELDS, TAX_FIELDS
 
 
-class Word():
-    def __init__(self, text):
-        self.text = self.clean(text)
-
-    def clean(self, text):
-        # remove any non alpha numeric characters, decimals and dash
-        return re.sub(r'[^a-zA-Z0-9.\-\%]', '', text)
-
-    def is_money(self):
-        pattern = re.compile('^(-?\$?(\d)*\.(\d))')
-        return pattern.match(self.text) is not None
-
-    def is_number(self):
-        try:
-            float(self.text)
-            return True
-        except ValueError:
-            return False
-
-    def is_percentage(self):
-        return '%' in self.text
-
-    def numeric_money_amount(self):
-        if not self.is_money():
-            return None
-
-        number = ''
-        pattern = re.compile('^\d')
-        for char in self.text:
-            if char == "." or pattern.match(char):
-                number = number + char
-
-        return Decimal(number)
-
 def scan(image_uri):
     # Instantiates a client
     client = vision.ImageAnnotatorClient()
