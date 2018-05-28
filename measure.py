f36a6cc7334fd8e8ebb1a77af47500fea9a5d7e6
Anton Nguyen <afnguyen85@gmail.com>
2018-05-28 13:38:25 -0400

    Reformatted the output to be easier to read and compare results

--- measure.py
+++ measure.py
@@ -1,46 +1,55 @@
 from decimal import Decimal
 from vision.scan import scan
 from prettytable import PrettyTable
+from termcolor import colored
 import csv
 import os
 import sys
 
-def check_receipt(image, expected_total):
-    try:
-        result = {
-            'status': 'ok',
-            'sub_total': 'Unknown',
-            'taxes': 'Unknown'
-        }
-        url = "http://afn85.webfactional.com/receipts/{}".format(image)
-        expense = scan(url)
+IMAGE_FIELD = 0
+VENDOR_FIELD = 1
+SUBTOTAL_FIELD = 2
+TAX1_NAME_FIELD = 3
+TAX1_AMOUNT_FIELD = 4
+TAX2_NAME_FIELD = 5
+TAX2_AMOUNT_FIELD = 6
+TAX3_NAME_FIELD = 7
+TAX3_AMOUNT_FIELD = 8
+TOTAL_FIELD = 9
 
-        result['sub_total'] = str(expense['sub_total'])
-        result['taxes'] = expense['taxes']
-        actual_total = str(expense['grand_total'])
-        result['total'] = {
-            'expected': expected_total,
-            'actual': expense['grand_total']
-        }
-        result['vendor'] = expense['vendor']
-        result['date'] = expense['date']
+expense_csv_mapping = {
+    'vendor': VENDOR_FIELD,
+    'sub_total': SUBTOTAL_FIELD,
+    'tax1_amount': TAX1_AMOUNT_FIELD,
+    'tax2_amount': TAX2_AMOUNT_FIELD,
+    'tax3_amount': TAX3_AMOUNT_FIELD,
+    'grand_total': TOTAL_FIELD
+}
 
-        if actual_total != expected_total:
-            if expected_total != "Fail" and actual_total != "0.00":
-                result['status'] = 'failed'
-            else:
-                result['total']['actual'] = 'Fail'
+def check_receipt(expected):
+    status = True
+    url = "http://afn85.webfactional.com/receipts/{}".format(expected[IMAGE_FIELD])
 
-        return result
-    except KeyboardInterrupt:
-        exit()
-    except:
-        result['total'] = {
-            'expected': expected_total,
-            'actual': 'Unknown'
-        }
-        result['status'] = 'failed'
-        return result
+    expense = scan(url)
+
+    num_fields = 0
+    num_correct = 0
+
+    for field, csv_field_index in expense_csv_mapping.items():
+        if not expected[csv_field_index]:
+            continue
+
+        num_fields += 1
+        if expected[csv_field_index].lower() == str(expense[field]).lower():
+            num_correct += 1
+            continue
+
+    if not num_correct:
+        if not expense['grand_total']:
+            return 100, expense
+        return 0, expense
+
+    return round(num_correct/num_fields * 100, 1), expense
 
 def main():
     image_override = None
@@ -50,50 +59,63 @@ def main():
     with open('measure.csv', 'r') as csvfile:
         num_success = 0
         num_receipts = 0
-        image = 0
-        total = 1
+
+        results_table = PrettyTable(['#', 'Image', 'Result', 'Vendor', 'Subtotal', 'Tax1', 'Tax2', 'Tax3', 'Total', 'Status'])
+        receipts = csv.reader(csvfile, delimiter=',', quotechar='"')
 
         print('Progress:')
         for i, receipt in enumerate(receipts):
-            if image_override and image_override != receipt[image]:
+            if image_override and image_override != receipt[IMAGE_FIELD]:
                     continue
+            text = colored("{}: ".format(receipt[IMAGE_FIELD]), 'blue')
+            print(text, end='')
+            status, result = check_receipt(receipt)
 
-            result = check_receipt(
-                receipt[image],
-                receipt[total]
-            )
+            num_success += status
 
             results_table.add_row([
                 i + 1,
-                receipt[image],
+                receipt[IMAGE_FIELD],
                 'Expected',
-                '',
-                '',
-                result['total']['expected']
-                result['status']
+                receipt[VENDOR_FIELD],
+                receipt[SUBTOTAL_FIELD],
+                receipt[TAX1_AMOUNT_FIELD],
+                receipt[TAX2_AMOUNT_FIELD],
+                receipt[TAX2_AMOUNT_FIELD],
+                receipt[TOTAL_FIELD],
+                "✔" if status == 1 else "{} %".format(status)
             ])
             results_table.add_row([
                 '',
                 '',
                 'Actual',
+                result['vendor'],
                 result['sub_total'],
-                result['taxes'],
-                result['total']['actual'],
+                result['tax1_amount'],
+                result['tax2_amount'],
+                result['tax2_amount'],
+                result['grand_total'],
                 ''
             ])
-            results_table.add_row(['','','','','','',''])
+            results_table.add_row(['-','-','-','-','-','-','-','-','-','-'])
 
             num_receipts = num_receipts + 1
-            if result['status'] == 'failed':
-                print('F', end='', flush=True)
+            if status != 100:
+                text = colored("{}%".format(status), 'red')
+                print(text, flush=True)
                 continue
 
-            num_success = num_success + 1
-            print('.', end='', flush=True)
+            print('✔', flush=True)
 
+
+        correct_percentage = (num_success / (num_receipts * 100)) * 100
+        results_table.add_row(['','','','','','','','','',''])
         results_table.add_row([
             'Results',
-            "{}/{} Succeeded".format(num_success, num_receipts),
+            "{}% Succeeded".format(round(correct_percentage, 1)),
+            '',
+            '',
+            '',
             '',
             '',
             '',
