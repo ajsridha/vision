fd43c5b4f720b2a90136988bc97a289512b6fec3
Anton Nguyen <afnguyen85@gmail.com>
2018-05-30 12:21:29 -0400

    Added proper unit tests

---
+++ test/__init__.py
@@ -0,0 +1,13 @@
+import logging
+from vision.scan import scan
+from unittest import TestCase
+
+logging.basicConfig()
+vcr_log = logging.getLogger("vcr")
+vcr_log.setLevel(logging.INFO)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+class BaseTestCase(TestCase):
+    def scan_expense(self, image):
+        url = "http://afn85.webfactional.com/receipts/{}".format(image)
+        return scan(url)
